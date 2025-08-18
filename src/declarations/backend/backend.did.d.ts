d19bbdcd1aa958430b969383dc470e925646e7a1
Rozaq <rozaqabdul678@gmail.com>
2025-08-18 08:44:15 +0700

    feat: smart contarct llm

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -16,13 +16,22 @@ export interface FunctionCall {
   'name' : string,
   'arguments' : Array<ToolCallArgument>,
 }
+export type Result = { 'Ok' : null } |
+  { 'Err' : string };
+export interface SavedAccount {
+  'alias' : string,
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
 export interface ToolCall { 'id' : string, 'function' : FunctionCall }
 export interface ToolCallArgument { 'value' : string, 'name' : string }
 export interface _SERVICE {
-  'chat' : ActorMethod<[Array<ChatMessage>], string>,
-  'create_account' : ActorMethod<[string], string>,
-  'get_accounts' : ActorMethod<[], Array<string>>,
-  'prompt' : ActorMethod<[string], string>,
+  'copilot_chat' : ActorMethod<[Array<ChatMessage>], string>,
+  'list_accounts' : ActorMethod<[], Array<SavedAccount>>,
+  'save_account' : ActorMethod<
+    [string, string, [] | [Uint8Array | number[]]],
+    Result
+  >,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
